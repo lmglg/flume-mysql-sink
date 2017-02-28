14bdff37047b0708f364cfc1ec9d7bf88dbd337c
miller.lee <youresky@gmail.com>
2017-02-28 21:46:49 +0800

    Add files via upload

--- src/main/java/org/flume/mysql/sink/MysqlSink.java
+++ src/main/java/org/flume/mysql/sink/MysqlSink.java
@@ -1,156 +1,169 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/** 
+ * Licensed to the Apache Software Foundation (ASF) under one 
+ * or more contributor license agreements.  See the NOTICE file 
+ * distributed with this work for additional information 
+ * regarding copyright ownership.  The ASF licenses this file 
+ * to you under the Apache License, Version 2.0 (the 
+ * "License"); you may not use this file except in compliance 
+ * with the License.  You may obtain a copy of the License at 
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+ */  
 package org.flume.mysql.sink;
 
 /**
  *
  * @author ml.li
  */
-import com.google.common.base.Preconditions;
-import com.google.common.base.Throwables;
-import com.google.common.collect.Lists;
-import org.apache.flume.*;
-import org.apache.flume.conf.Configurable;
-import org.apache.flume.sink.AbstractSink;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
- 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
- 
-public class MysqlSink extends AbstractSink implements Configurable {
- 
-    private Logger LOG = LoggerFactory.getLogger(MysqlSink.class);
-    private String hostname;
-    private String port;
-    private String databaseName;
-    private String tableName;
-    private String user;
-    private String password;
-    private PreparedStatement preparedStatement;
-    private Connection conn;
-    private int batchSize;
- 
-    public MysqlSink() {
-        LOG.info("MysqlSink start...");
-    }
- 
-    public void configure(Context context) {
-        hostname = context.getString("hostname");
-        Preconditions.checkNotNull(hostname, "hostname must be set!!");
-        port = context.getString("port");
-        Preconditions.checkNotNull(port, "port must be set!!");
-        databaseName = context.getString("databaseName");
-        Preconditions.checkNotNull(databaseName, "databaseName must be set!!");
-        tableName = context.getString("tableName");
-        Preconditions.checkNotNull(tableName, "tableName must be set!!");
-        user = context.getString("user");
-        Preconditions.checkNotNull(user, "user must be set!!");
-        password = context.getString("password");
-        Preconditions.checkNotNull(password, "password must be set!!");
-        batchSize = context.getInteger("batchSize", 100);
-        Preconditions.checkNotNull(batchSize > 0, "batchSize must be a positive number!!");
-    }
- 
-    @Override
-    public void start() {
-        super.start();
-        try {
-            //调用Class.forName()方法加载驱动程序
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
- 
-        String url = "jdbc:mysql://" + hostname + ":" + port + "/" + databaseName; 
-        //调用DriverManager对象的getConnection()方法，获得一个Connection对象
- 
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            conn.setAutoCommit(false);
-            //创建一个Statement对象
-            preparedStatement = conn.prepareStatement("insert into " + tableName + 
-                                               " (content) values (?)");
- 
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
- 
-    }
- 
-    @Override
-    public void stop() {
-        super.stop();
-        if (preparedStatement != null) {
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
- 
-        if (conn != null) {
-            try {
-                conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
- 
-    public Status process() throws EventDeliveryException {
-        Status result = Status.READY;
-        Channel channel = getChannel();
-        Transaction transaction = channel.getTransaction();
-        Event event;
-        String content;
- 
-        List<String> actions = Lists.newArrayList();
-        transaction.begin();
-        try {
-            for (int i = 0; i < batchSize; i++) {
-                event = channel.take();
-                if (event != null) {
-                    content = new String(event.getBody());
-                    actions.add(content);
-                } else {
-                    result = Status.BACKOFF;
-                    break;
-                }
-            }
- 
-            if (actions.size() > 0) {
-                preparedStatement.clearBatch();
-                for (String temp : actions) {
-                    preparedStatement.setString(1, temp);
-                    preparedStatement.addBatch();
-                }
-                preparedStatement.executeBatch();
- 
-                conn.commit();
-            }
-            transaction.commit();
-        } catch (Throwable e) {
-            try {
-                transaction.rollback();
-            } catch (Exception e2) {
-                LOG.error("Exception in rollback. Rollback might not have been" +
-                        "successful.", e2);
-            }
-            LOG.error("Failed to commit transaction." +
-                    "Transaction rolled back.", e);
-            Throwables.propagate(e);
-        } finally {
-            transaction.close();
-        }
- 
-        return result;
-    }
-}
+import com.google.common.base.Preconditions;  
+import com.google.common.base.Throwables;  
+import com.google.common.collect.Lists;  
+import org.apache.flume.*;  
+import org.apache.flume.conf.Configurable;  
+import org.apache.flume.sink.AbstractSink;  
+import org.slf4j.Logger;  
+import org.slf4j.LoggerFactory;  
+   
+import java.sql.Connection;  
+import java.sql.DriverManager;  
+import java.sql.PreparedStatement;  
+import java.sql.SQLException;  
+import java.util.List;  
+   
+public class MysqlSink extends AbstractSink implements Configurable {  
+   
+    private Logger LOG = LoggerFactory.getLogger(MysqlSink.class);  
+    private String hostname;  
+    private String port;  
+    private String databaseName;  
+    private String tableName;  
+    private String user;  
+    private String password;  
+    private PreparedStatement preparedStatement;  
+    private Connection conn;  
+    private int batchSize;  
+   
+    public MysqlSink() {  
+        LOG.info("MysqlSink start...");  
+    }  
+   
+    public void configure(Context context) {  
+        hostname = context.getString("hostname");  
+        Preconditions.checkNotNull(hostname, "hostname must be set!!");  
+        port = context.getString("port");  
+        Preconditions.checkNotNull(port, "port must be set!!");  
+        databaseName = context.getString("databaseName");  
+        Preconditions.checkNotNull(databaseName, "databaseName must be set!!");  
+        tableName = context.getString("tableName");  
+        Preconditions.checkNotNull(tableName, "tableName must be set!!");  
+        user = context.getString("user");  
+        Preconditions.checkNotNull(user, "user must be set!!");  
+        password = context.getString("password");  
+        Preconditions.checkNotNull(password, "password must be set!!");  
+        batchSize = context.getInteger("batchSize", 100);  
+        Preconditions.checkNotNull(batchSize > 0, "batchSize must be a positive number!!");  
+    }  
+   
+    @Override  
+    public void start() {  
+        super.start();  
+        try {  
+            //璋冪敤Class.forName()鏂规硶鍔犺浇椹卞姩绋嬪簭  
+            Class.forName("com.mysql.jdbc.Driver");  
+        } catch (ClassNotFoundException e) {  
+            e.printStackTrace();  
+        }  
+   
+        String url = "jdbc:mysql://" + hostname + ":" + port + "/" + databaseName;   
+        //璋冪敤DriverManager瀵硅薄鐨刧etConnection()鏂规硶锛岃幏寰椾竴涓狢onnection瀵硅薄  
+   
+        try {  
+            conn = DriverManager.getConnection(url, user, password);  
+            conn.setAutoCommit(false);  
+            //鍒涘缓涓�涓猄tatement瀵硅薄  
+            preparedStatement = conn.prepareStatement("insert into " + tableName +   
+                                               " (content) values (?)");  
+   
+        } catch (SQLException e) {  
+            e.printStackTrace();  
+            System.exit(1);  
+        }  
+   
+    }  
+   
+    @Override  
+    public void stop() {  
+        super.stop();  
+        if (preparedStatement != null) {  
+            try {  
+                preparedStatement.close();  
+            } catch (SQLException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+   
+        if (conn != null) {  
+            try {  
+                conn.close();  
+            } catch (SQLException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+    }  
+   
+    public Status process() throws EventDeliveryException {  
+        Status result = Status.READY;  
+        Channel channel = getChannel();  
+        Transaction transaction = channel.getTransaction();  
+        Event event;  
+        String content;  
+   
+        List<String> actions = Lists.newArrayList();  
+        transaction.begin();  
+        try {  
+            for (int i = 0; i < batchSize; i++) {  
+                event = channel.take();  
+                if (event != null) {  
+                    content = new String(event.getBody());  
+                    actions.add(content);  
+                } else {  
+                    result = Status.BACKOFF;  
+                    break;  
+                }  
+            }  
+   
+            if (actions.size() > 0) {  
+                preparedStatement.clearBatch();  
+                for (String temp : actions) {  
+                    preparedStatement.setString(1, temp);  
+                    preparedStatement.addBatch();  
+                }  
+                preparedStatement.executeBatch();  
+   
+                conn.commit();  
+            }  
+            transaction.commit();  
+        } catch (Throwable e) {  
+            try {  
+                transaction.rollback();  
+            } catch (Exception e2) {  
+                LOG.error("Exception in rollback. Rollback might not have been" +  
+                        "successful.", e2);  
+            }  
+            LOG.error("Failed to commit transaction." +  
+                    "Transaction rolled back.", e);  
+            Throwables.propagate(e);  
+        } finally {  
+            transaction.close();  
+        }  
+   
+        return result;  
+    }  
+}  
+
